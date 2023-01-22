07f35f138348531a9c71bf8e5b578a4cacaccd24
Dhairya RPi <dtriv97@gmail.com>
2023-01-22 17:05:31 +1300

    Basic update

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const http = require('http');
-const hostname = '192.168.86.30';
+const exec = require("child_process").exec;
+
+// NOTE: This is the Raspberry Pi's local IP Address
+const hostname = '192.168.86.24';
+
+// Port to listen/send on the IP Address
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
+    console.log("Request received.");
+    res.writeHead(200, {"Content-Type" : "text/plain"})
     res.end('Hello World!');
 });
 
